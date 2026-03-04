4a02342c88c15c69747a4099f8c75414398b8e29
Abu <blogchikuz@gmail.com>
2026-03-04 07:11:43 +0500

    Remove PostCSS and TypeScript configuration files

--- eslint.config.mjs
+++
@@ -1,6 +0,0 @@
-import nextConfig from "eslint-config-next";
-import nextTypescript from "eslint-config-next/typescript";
-
-const eslintConfig = [...nextConfig, ...nextTypescript];
-
-export default eslintConfig;
