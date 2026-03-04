2dcc2f893b2a709376b029ca1d7ac2cc11868e50
Abu <blogchikuz@gmail.com>
2026-03-04 11:57:53 +0500

    feat: add Blog page with metadata and initial content

--- src/components/navigation-menu.tsx
+++ src/components/navigation-menu.tsx
@@ -10,6 +10,7 @@ const navLinks = [
   { label: "Home", href: "/" },
   { label: "About", href: "/about" },
   { label: "Projects", href: "/projects" },
+  { label: "Blog", href: "/blog" },
   { label: "Contact", href: "/contact" },
 ];
 
